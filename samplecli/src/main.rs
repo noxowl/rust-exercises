63d72f2c517136ebf89625f3be4a6ec98f1d601b
Suyeong RHIE <10545351+noxowl@users.noreply.github.com>
2022-10-11 13:50:02 +0900

    Add test

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -132,3 +132,29 @@ impl RpnCalculator {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ok() {
+        let calc = RpnCalculator::new(false);
+        assert_eq!(calc.eval("4"), 4);
+        assert_eq!(calc.eval("42"), 42);
+        assert_eq!(calc.eval("-42"), -42);
+
+        assert_eq!(calc.eval("4 2 +"), 6);
+        assert_eq!(calc.eval("4 2 *"), 8);
+        assert_eq!(calc.eval("2 3 -"), -1);
+        assert_eq!(calc.eval("2 3 /"), 0);
+        assert_eq!(calc.eval("2 3 %"), 2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_ng() {
+        let calc = RpnCalculator::new(false);
+        calc.eval("1 1 ^");
+    }
+}
